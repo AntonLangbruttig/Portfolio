e814c2555496ccd4379311871d8e905093221926
Anton Langbruttig <antonLangbruttig@gmail.com>
2025-03-13 17:33:02 -0500

    Converted some images to webp so they will load faster

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function HomePage() {
             <div className="flex items-center p-3 sm:h-[50%] sm:p-3  sm:w-screen md:h-auto md:w-1/2 md:mb-12 md:-ml-8 lg:mb-12 lg:-ml-8">
               <div className="relative w-full h-full sm:mt-4 md:h-[507px] sm:mr-1 md:mr-0 md:ml-2 md:mt-0">
                 <Image
-                  src="/images/me.png"
+                  src="/images/me.webp"
                   alt="Anton Langbruttig"
                   layout="fill"
                   objectFit="contain"
