ae0f350bc9395f522082b3e8c560b440dd8422d8
Anton Langbruttig <antonLangbruttig@gmail.com>
2025-03-13 15:45:31 -0500

    more up dates to get images to work

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ export default {
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://api.iconify.design;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
               font-src 'self' https://fonts.gstatic.com;
-              img-src 'self' data: https://api.iconify.design;
+              img-src 'self' data: https://api.iconify.design https://main.d2l6i0f7oy0lzy.amplifyapp.com https://*.amazonaws.com;
               connect-src 'self' https://api.iconify.design;
               frame-src 'none';
             `.replace(/\s{2,}/g, " "),
